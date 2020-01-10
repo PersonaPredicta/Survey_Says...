dd19b79754dea532cc3ceadb14cbcf92627da0cb
Fredrick Lambuth <flambuth@gmail.com>
2020-01-10 10:47:44 -0600

    nlp has gotten more basic

--- fred/nlp.py
+++ fred/nlp.py
@@ -4,14 +4,32 @@ from textblob import TextBlob
 import pandas as pd
 from itertools import islice
 
+#Each function will be applied to the columns with the Series.apply() method 
 
-def create_sentiment_scores(orig_text):
+def find_polarity(input_text):
     """
-    Accepts a Series/df_column that of text responses. Outputs the polarity, subjectivity, and other scores 
-    in a new dataframe.
+    Takes one document and outputs a polarity score. -1 is Very Negative, 1 is Super Positive
+    This function should be applied to a column and outputted to a new column.
     """
+    return TextBlob(input_text).sentiment.polarity
 
-    pass
+def find_subjectivity(input_text):
+    """
+    Takes one document and outputs a subjectivity score. 0 is cold objectivity, 1 is very subjective
+    This function should be applied to a column and outputted to a new column.
+    """
+    return TextBlob(input_text).sentiment.subjectivity
+
+
+
+
+# def create_sentiment_scores(orig_text):
+#     """
+#     Accepts a Series/df_column that of text responses. Outputs the polarity, subjectivity, and other scores 
+#     in a new dataframe.
+#     """
+
+#     pass
 
 #I'm just gonna write a few two line functions that append to a blank DF
     # for index, row in islice(orig_df.iterrows(), 0, None):
