6f543aa3348f3958c8f08d09dcf465fd01fe9bcc
Fredrick Lambuth <flambuth@gmail.com>
2020-01-16 11:48:57 -0600

    added lemmatize function to nlp

--- nlp.py
+++ nlp.py
@@ -119,41 +119,11 @@ def show_pyLDAvis_dashboard(lda_fitted_model, doc_term_matrix, vector):
     pyLDAvis.sklearn.prepare(lda_fitted_model, doc_term_matrix, vector)
 
 
-def make_word_counts(input_column):
+def lemmatize(text):
     """
-    I should probably stop at the doc term matrix.
+    accept some text and return the text after applying lemmatization to each word.
+    Use with .apply to a Pandas Series
     """
-#words that appear in less than 80% of the document and appear in at least 2 documents. Also removes the stop
-#words that are in sklearns stopword list.
-    count_vect = CountVectorizer(max_df=0.8, min_df=2, stop_words='english')
-#The doc term matrix is a sparse matrix that has as many columns as there are words in the corpus/input_column.
-#count_vect is now 'fitted' to the corpus we gave it. It has some useful methods and attributes to use later.
-    doc_term_matrix = count_vect.fit_transform(input_column.values.astype('U'))
-#I'm unsure about the astype('U'), but it was in the tutorial I copied this stuff from
-
-#n_components defines how many topic/clusters will be sorted. 
-    LDA = LatentDirichletAllocation(n_components=5, random_state=42)
-    LDA.fit(doc_term_matrix)
-
-#LDA, after fitting to the document term matrix, is an array of 5. Each element is a topic, with a probability of
-# each word/feature/column in the doc_term_matrix    
-    first_topic = LDA.components_[0]
-#This is the top 5 words/features in the first topic. It returns the index, so we have to look up the actual
-#word in the count_vect object
-    first_topic.argsort()[-5:]
-#loops through the 5 words, and passes them to the count_vect.get_feature_name method
-    for i in first_topic.argsort()[-5:]: 
-        print(count_vect.get_feature_names()[i])
-
-# Loops through the 5 topics, which are the LDA.components. 
-# The inner loop is is the loop above this one 
-    for i, topic in enumerate(LDA.components_): 
-        print(f"Top 5 words for topic #{i}:") 
-        print([count_vect.get_feature_names()[i] for i in topic.argsort()[-5:]]) 
-        print('\n')
-#These are an ndarray that is as wide as the amount of topics defined by LDA. Each value in each vector
-#is the probability for each topic.
-    topic_values = LDA.transform(doc_term_matrix)
-#This will select the value in each row with the highest probability. The best guess for what its topic is.
-    topic_values.argmax(axis=1)
-    pass
+    wnl = nltk.stem.WordNetLemmatizer()
+    lemmas = [wnl.lemmatize(word) for word in text.split()]
+    return ' '.join(lemmas)
